6aa2bca5eaf35ca9adf7fb3f220b532ea60ef6f4
Jason Gantenberg <jason.gantenberg@gmail.com>
2017-04-02 13:26:22 -0400

    RStudio
    Add option to cite RStudio

--- function.R
+++ function.R
@@ -1,10 +1,29 @@
-citeRpacks <- function(pkg_list, filename) {
-  
+citeRpacks <- function(pkg_list, filename, RStudio = FALSE) {
+
   #ht to https://stackoverflow.com/questions/2470248/write-lines-of-text-to-a-file-in-r for sink()
   for (i in 1:length(pkg_list)) {
       sink(file = paste(filename, ".bib", sep = ""), append = T)
       writeLines(toBibtex(citation(package = pkg_list[i])))
       sink()
   }
-  
+
+  if(RStudio) {
+
+    c <- RStudio.Version()$citation
+
+    sink(file = paste(filename, ".bib", sep = ""), append = T)
+    writeLines(paste("@Manual{,",
+                     "\n   title = {", c$title, "},",
+                     "\n   author = {{", c$author, "}},",
+                     "\n   organization = {", c$organization, "},",
+                     "\n   address = {", c$address, "},",
+                     "\n   year = {", c$year, "},",
+                     "\n   note = {", RStudio.Version()$version, "}",
+                     "\n   url = {", c$url, "},",
+                     "\n }",
+                     sep = ""))
+    sink()
+
+  }
+
 }
